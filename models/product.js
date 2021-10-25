b215ae4f52c71b364aec87ce8d28f054a9f86307
Markos <markosmk@me.com>
2021-10-25 17:47:25 -0300

    [update] all fields schema products

--- models/product.js
+++ models/product.js
@@ -1,14 +1,60 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
-  name: String,
-  content: String,
-  image: String,
-  date: { type: Date, default: Date.now },
-  hidden: Boolean,
-  meta: {
-    votes: Number,
-    favs: Number,
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  richDescription: {
+    type: String,
+    default: '',
+  },
+  image: {
+    type: String,
+    default: '',
+  },
+  images: [
+    {
+      type: String,
+    },
+  ],
+  brand: {
+    type: String,
+    default: '',
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  stock: {
+    type: Number,
+    default: 0,
+    min: 0,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  numReviews: {
+    type: Number,
+    default: 0,
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
   },
 });
 
